4ee73aba4139e7f20b45fe46373598649a6d6d59
Neiland <rodrigo.naohan@gmail.com>
2026-04-10 12:15:06 -0300

    1° commit

---
+++ src/main/java/ananditos/sandraxandreia/SandraXAndreiaApplication.java
@@ -0,0 +1,13 @@
+package ananditos.sandraxandreia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandraXAndreiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandraXAndreiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ananditos/sandraxandreia/SandraXAndreiaApplicationTests.java
@@ -0,0 +1,13 @@
+package ananditos.sandraxandreia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandraXAndreiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
